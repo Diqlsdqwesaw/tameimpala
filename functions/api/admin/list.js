0c539b370e53435f04b4c246da29df136b03cea5
Diqlsdqwesaw <121684123+Diqlsdqwesaw@users.noreply.github.com>
2025-10-11 15:05:42 +0200

    Delete functions/api/admin directory

--- functions/api/admin/list.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet({ env }) {
-  try {
-    const list = await env.INVITES.list({ prefix: '' });
-    const invites = list.keys.map(k => k.name);
-    return Response.json({ invites, error: null });
-  } catch (error) {
-    return Response.json({ invites: [], error: error.message }, { status: 500 });
-  }
-}
